9482b6911a25d84385d9ba5a8e1ba2844951286a
Gareth Cross <gcross.code@icloud.com>
2015-03-05 16:15:12 -0500

    Added laser altimeter

---
+++ laser_altimeter/src/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * main.cpp
+ *  _   _             _           _____         _
+ * | \ | | ___  _   _| | ____ _  |_   _|__  ___| |__
+ * |  \| |/ _ \| | | | |/ / _` |   | |/ _ \/ __| '_ \
+ * | |\  | (_) | |_| |   < (_| |   | |  __/ (__| | | |
+ * |_| \_|\___/ \__,_|_|\_\__,_|   |_|\___|\___|_| |_|
+ *
+ *  Copyright (c) 2014 Nouka Technologies. All rights reserved.
+ *
+ *  This file is part of laser_altimeter.
+ *
+ *	Created on: 24/08/2014
+ */
+
+#include <ros/ros.h>
+#include <laser_altimeter/node.hpp>
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "laser_altimeter");
+  ros::NodeHandle nh, pnh("~");
+  try {
+  galt::laser_altimeter::Node node(nh, pnh);
+  ros::spin();
+  }
+  catch (const std::exception& e) {
+   ROS_ERROR("%s: %s", pnh.getNamespace().c_str(), e.what()) ;
+  }
+}
